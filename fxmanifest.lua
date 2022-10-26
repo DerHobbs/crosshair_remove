d3dab783942d9fd4550000aa5bf8bbe5a5235b14
Hobbs <101003021+DerHobbs@users.noreply.github.com>
2022-10-27 00:02:02 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games {'rdr3'}
+
+author 'https://discord.gg/uCG9pbfdah'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+this_is_a_map 'yes'
